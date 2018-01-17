04c17c6ccb9c87828c6c4c86dbb9147b101f1772
Lucas Tafarel <lucastafarelbs@gmail.com>
2018-01-17 10:39:40 -0200

    issue on users

--- src/domains/users/business-rules/get-all.js
+++ src/domains/users/business-rules/get-all.js
@@ -1,3 +1,9 @@
-const getAll = require('./get.js').getAll
+const GetModel = require('../../models/get-model.js')
+
+const getAll = async ( req, query={}, options ) => {
+  const model = GetModel( req.$connection, req.$model )
+  const docs = await model.find( query )
+  return docs || []
+}
 
 module.exports = { getAll }
